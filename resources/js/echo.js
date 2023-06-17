d2d98a1dd0a7ce4528ab757fcdb5e93820d4c6a7
P-Lam <59561008+PressSay@users.noreply.github.com>
2023-06-17 22:11:46 +0700

    Update echo.js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -8,8 +8,6 @@ window.Echo = new Echo({
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    // wsHost: '10.13.132.48',
-    // wsHost: '192.168.1.6',
     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 6001,
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
